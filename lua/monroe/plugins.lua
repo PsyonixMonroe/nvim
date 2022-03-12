1322d58d2906c236ca5c653bcafbf5f08d5cf3a5
Matt Monroe <matt.monroe@psyonix.com>
2022-03-11 16:58:44 -0800

    setting up lsp

--- lua/monroe/plugins.lua
+++ lua/monroe/plugins.lua
@@ -2,24 +2,36 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+-- standard plugins 
 Plug 'mhinz/vim-signify'
 Plug 'tpope/vim-surround'
 Plug 'tpope/vim-commentary'
 
+-- useful as libraries
 Plug 'nvim-lua/popup.nvim'
 Plug 'nvim-lua/plenary.nvim'
 
+-- colorscheme 
 Plug 'lunarvim/colorschemes'
 
+-- CMP 
 Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-cmdline'
 Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'hrsh7th/cmp-nvim-lsp'
 
 Plug 'L3MON4D3/LuaSnip'
 Plug 'rafamadriz/friendly-snippets'
 
+-- LSP 
+Plug 'neovim/nvim-lspconfig'
+Plug 'williamboman/nvim-lsp-installer'
+
+-- get rid of??
 Plug 'junegunn/fzf'
+
+
 
 vim.call('plug#end')
