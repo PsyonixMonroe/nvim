031b0935d1a554a91e45bb48a38d3cf9f02d6fe1
Matt Monroe <matt.monroe@psyonix.com>
2022-04-29 12:06:57 -0700

    fix duplicate newlines at the end of files

--- lua/monroe/options.lua
+++ lua/monroe/options.lua
@@ -29,6 +29,7 @@ vim.opt.numberwidth = 4                         -- set number column width to 2 
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
+vim.opt.binary = true                           -- don't automatically add new line to end of file on save
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "Hack 10"                     -- the font used in graphical neovim applications
 
