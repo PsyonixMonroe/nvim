7dff5c9b53231df141b6f3a502ef71c167ae857c
Matt Monroe <matt.monroe@psyonix.com>
2022-07-05 11:16:41 -0700

    add git-conflict plugint

--- lua/monroe/conflict.lua
+++ lua/monroe/conflict.lua
@@ -1,18 +1,18 @@
--- local status, conflict = pcall(require, "git-conflict")
--- if not status then
---   print("Git-Conflict Not Loaded")
---   return
--- end
---
--- print("Git-Conflict Loaded")
--- conflict.setup({
---   default_mappings = true,
---   disable_diagnostics = false,
---   highlights = {
---     incoming = 'DiffText',
---     current = 'DiffAdd',
---   }
--- })
+ local status, conflict = pcall(require, "git-conflict")
+ if not status then
+   print("Git-Conflict Not Loaded")
+   return
+ end
 
-require "git-conflict".setup()
+ print("Git-Conflict Loaded")
+ conflict.setup({
+   default_mappings = true,
+   disable_diagnostics = false,
+   highlights = {
+     incoming = 'DiffText',
+     current = 'DiffAdd',
+   }
+ })
+
+-- require "git-conflict".setup()
 
