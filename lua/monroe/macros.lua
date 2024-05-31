58ec1c141056eb1ca6f4e53ea9ba3380c515d975
Matt Monroe <matt.monroe@psyonix.com>
2024-05-31 11:14:08 -0600

    update error macro to properly indent the lines

--- lua/monroe/macros.lua
+++ lua/monroe/macros.lua
@@ -1,7 +1,7 @@
 vim.keymap.set(
 	"n",
 	"<leader>ee",
-	"oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+	"oif err != nil {<CR>}<Esc>Oreturn err<Esc>>%"
 )
 
 vim.keymap.set("n", "<C-f>", function() vim.lsp.buf.format() end)
